43d2a2009ba95ddd0ae12a64fe9c97d5ac3fe917
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-04-06 20:53:35 +0300

    added createTable function

--- src/main/java/database/ProductDAO.java
+++ src/main/java/database/ProductDAO.java
@@ -4,14 +4,15 @@ import org.postgresql.ds.PGSimpleDataSource;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ProductDAO {
-    private String URL = "jdbc:postgresql://localhost:5432/postgres?loggerLevel=OFF";
     private String TABLE_NAME = "Products";
     private PGSimpleDataSource dataSource;
 
     public ProductDAO(String user, String password) {
         dataSource = new PGSimpleDataSource();
+        String URL = "jdbc:postgresql://localhost:5432/postgres?loggerLevel=OFF";
         dataSource.setUrl(URL);
         dataSource.setServerName("localhost");
         dataSource.setPortNumber(5432);
@@ -33,10 +34,16 @@ public class ProductDAO {
     }
 
     private void createTable() {
-
-    }
-
-    public void clearTable() {
-
+        try (Connection connection = dataSource.getConnection()) {
+            Statement statement = connection.createStatement();
+            statement.execute("CREATE TABLE IF NOT EXISTS  " + TABLE_NAME + "(" +
+                    "id SERIAL NOT NULL, " +
+                    "prodid VARCHAR(36) NOT NULL, " +
+                    "title VARCHAR(40) NOT NULL, " +
+                    "cost INTEGER NOT NULL, " +
+                    "PRIMARY KEY (id), UNIQUE(prodid), UNIQUE(title))");
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to create table.", e);
+        }
     }
 }
