48d169bd83b30884b59d9909257f1c1337331a28
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-04-06 19:38:51 +0300

    connection to DB with DataSource

--- src/main/java/database/ProductDAO.java
+++ src/main/java/database/ProductDAO.java
@@ -1,23 +1,42 @@
 package database;
 
+import org.postgresql.ds.PGSimpleDataSource;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ProductDAO {
     private String URL = "jdbc:postgresql://localhost:5432/postgres?loggerLevel=OFF";
     private String TABLE_NAME = "Products";
+    private PGSimpleDataSource dataSource;
 
     public ProductDAO(String user, String password) {
+        dataSource = new PGSimpleDataSource();
+        dataSource.setUrl(URL);
+        dataSource.setServerName("localhost");
+        dataSource.setPortNumber(5432);
+        dataSource.setDatabaseName("postgres");
+        dataSource.setUser(user);
+        dataSource.setPassword(password);
+
         try {
             Class.forName("org.postgresql.Driver");
         } catch (ClassNotFoundException e) {
             throw new RuntimeException("PostgreSQL JDBC Driver is not found.", e);
         }
-        try (Connection connection = DriverManager.getConnection(URL, user, password)) {
+        try (Connection connection = dataSource.getConnection()) {
         } catch (SQLException e) {
             throw new RuntimeException("Connection to Database Failed!", e);
         }
+
+        createTable();
     }
 
+    private void createTable() {
+
+    }
+
+    public void clearTable() {
+
+    }
 }
