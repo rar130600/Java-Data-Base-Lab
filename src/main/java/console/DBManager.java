339781f5512061d4567f4cac6135d27ad4db7566
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-04-07 22:46:07 +0300

    function execute command update README.md

--- src/main/java/console/DBManager.java
+++ src/main/java/console/DBManager.java
@@ -28,9 +28,40 @@ public class DBManager {
     }
 
     public void start() {
-        while(!login()){}
+        while (!login()) {}
 
-        reset();
+        try {
+            reset();
+        } catch (Exception e) {
+            out.println(e.getMessage());
+            out.println("Failed to reset table! :с\n");
+        }
+
+        out.println("Please enter commands");
+        out.println("Type \"/help\" for more information about commands");
+        while (in.hasNextLine()) {
+            String cmd = in.nextLine().trim();
+            if (cmd.equals("/exit")) {
+                break;
+            }
+            if (cmd.equals("/help")) {
+                help();
+                continue;
+            }
+            execute(cmd);
+        }
+    }
+
+    private void execute(String line) {
+        Scanner scanner = new Scanner(line);
+        if (scanner.hasNext()) {
+            try {
+                commands.getOrDefault(scanner.next(),
+                        (a) -> out.println("Unknown command.")).accept(scanner);
+            } catch (Exception e) {
+                out.println(e.getMessage());
+            }
+        }
     }
 
     private boolean login() {
@@ -45,6 +76,9 @@ public class DBManager {
         } catch (RuntimeException e) {
             out.println(e.getMessage());
             out.println(e.getCause().getMessage());
+            return false;
+        } catch (Exception e) {
+            out.println(e.getMessage());
             return false;
         }
         return true;
@@ -66,6 +100,15 @@ public class DBManager {
             dao.addToTable(new Product(i, "Товар" + i, i * 10));
         }
         out.println("Table is successfully filled!\n");
+    }
+
+    private void help() {
+        out.println("/add - add to table");
+        out.println("/delete - delete product from table");
+        out.println("/show_all - show all product in table");
+        out.println("/price - show price product");
+        out.println("/change_price - change price product");
+        out.println("/filter_by_price - show product in price range");
     }
 
     private void add(Scanner args){}
