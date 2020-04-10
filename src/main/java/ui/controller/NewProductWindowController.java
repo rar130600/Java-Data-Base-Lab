3c740772424bea6a65cc96973071e11491483bd4
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-04-10 22:39:46 +0300

    new product window

---
+++ src/main/java/ui/controller/NewProductWindowController.java
@@ -0,0 +1,30 @@
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class NewProductWindowController {
+
+    @FXML
+    private TextField fieldTitle;
+
+    @FXML
+    private TextField fieldPrice;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnAdd;
+
+    @FXML
+    void onClickAdd() {
+
+    }
+
+    @FXML
+    void onClickCancel() {
+
+    }
+}
