4dbd09765985cef10d1515bf4ac1bda7f1409727
Aleksander Rakhmatullin <rar130600@mail.ru>
2020-04-10 22:54:43 +0300

    update product window

---
+++ src/main/java/ui/controller/UpdatePriceWindowController.java
@@ -0,0 +1,30 @@
+package ui.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class UpdatePriceWindowController {
+    @FXML
+    private Text textProductTitle;
+
+    @FXML
+    private TextField fieldNewPrice;
+
+    @FXML
+    private Button btnCancel;
+
+    @FXML
+    private Button btnUpdate;
+
+    @FXML
+    void onClickCancel() {
+
+    }
+
+    @FXML
+    void onClickUpdate() {
+
+    }
+}
